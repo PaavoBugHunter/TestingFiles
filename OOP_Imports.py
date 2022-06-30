d93d2002e8ef21ba9c85d7dd06ec06667c07f4a4
Paavo Vuorilehto <paavo.vuorilehto@valagroup.com>
2022-06-30 17:09:39 +0300

    module import

---
+++ OOP_Imports.py
@@ -0,0 +1,5 @@
+from OOP_Cars import Cars #Write only attributes and methods to a class. Don't make instances with them.
+
+audi = Cars("Audi", "R8", 2021)
+audi.vehicle_description()
+audi.read_odometer()
