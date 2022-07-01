d10e8b2f8ac1a5842ab4a889e1a8d4776892b02e
Paavo Vuorilehto <paavo.vuorilehto@valagroup.com>
2022-07-01 12:00:21 +0300

    Class-imports to module

--- OOP_Imports.py
+++ OOP_Imports.py
@@ -1,5 +1,13 @@
-from OOP_Cars import Cars #Write only attributes and methods to a class. Don't make instances with them.
+from OOP_Cars_Module import Cars, ElectricCar #Write only attributes and methods to a class. Don't make instances with them.
 
 audi = Cars("Audi", "R8", 2021)
-audi.vehicle_description()
+print(audi.vehicle_description())
 audi.read_odometer()
+
+print("\n")
+
+polestar = ElectricCar("Polestar", "Northstar", 2022)
+print(polestar.vehicle_description())
+polestar.battery.describe_battery()
+polestar.battery.upgrade_battery(12)
+polestar.battery.estimate_range()
