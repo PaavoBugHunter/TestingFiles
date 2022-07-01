761659a0813a4789920c23a7de87183ce981c8c5
Paavo Vuorilehto <paavo.vuorilehto@valagroup.com>
2022-07-01 11:20:29 +0300

    Class-attributes with default values

--- OOP_Cars_Module.py
+++ OOP_Cars_Module.py
@@ -24,9 +24,9 @@ class Cars:
 
 
 class Battery():
-    '''Simple class to describe a 65-kWh-battery and its behaviors'''
+    '''Simple class to describe a battery and its behaviors'''
     def __init__(self):
-        self.battery_reserve = 65
+        self.battery_reserve = 75 #No need parameterize battery_reserve in the init-function since every battery-instance has default-charge of 75kWh
 
     def describe_battery(self):
         '''Print how much reserve the battery has'''
@@ -59,22 +59,3 @@ class ElectricCar(Cars): #"Cars" in parenthesis indicates ElectricCar-class to b
         '''
         super().__init__(make, model, year) #The super()-function imports all attributes and methods from Cars-class to ElectricCars-class. Alternative expression here is Cars.__init__(self, make, model, year):
         self.battery = Battery() #Establishes an instance of Battery-class as an attribute of ElectricCar
-
-my_tesla = ElectricCar("Tesla", "Model S", "2020")
-my_tesla.battery.battery_reserve = 74
-print(my_tesla.vehicle_description())
-my_tesla.battery.describe_battery()
-my_tesla.battery.estimate_range()
-my_tesla.battery.upgrade_battery(10)
-my_tesla.battery.estimate_range()
-
-print("\n")
-
-electric_mercedes = ElectricCar("Mercedes", "EQS", 2022)
-electric_mercedes.battery.battery_reserve = 60
-print(electric_mercedes.vehicle_description())
-electric_mercedes.battery.describe_battery()
-electric_mercedes.battery.estimate_range()
-electric_mercedes.battery.upgrade_battery(15)
-electric_mercedes.battery.estimate_range()
-
